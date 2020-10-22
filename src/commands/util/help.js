e79a07e00a2a3505821aa9fe1f47d3981a78709a
DomDevelops <65183965+DomDevelops@users.noreply.github.com>
2020-10-22 02:03:46 -0400

    Update help.js

--- src/commands/util/help.js
+++ src/commands/util/help.js
@@ -1,25 +1,8 @@
 const { MessageEmbed } = require('discord.js');
 const prefix = "/"
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
 
-    if(args[0] == "help") return message.channel.send(`Just do ${prefix}help instead.`)
-
-    if(args[0]) {
-        let command = args[0];
-        if(bot.commands.has(command)) {
-            command = bot.commands.get(command);
-            var SHembed = new MessageEmbed()
-            .setColor("#63798")
-            .setAuthor(`Sakura X`, message.guild.iconURL)
-            .setThumbnail(bot.user.displayAvatarURL)
-            .setDescription(`The bot prefix is: ${prefix}\n\n**Command:** ${command.name}\n**Description:** ${command.description || "No Description"}\n**Usage:** ${command.usage || "No Usage"}\n**Aliases:** ${command.aliases}`)
-            .setFooter("Sakura X || xFallen54x#2399", bot.user.displayAvatarURL)
-            message.channel.send(SHembed);
-        }}
-
-    if(!args[0]) {
-        message.delete();
         let embed = new MessageEmbed()
         .setAuthor(`Help Command!`, message.guild.iconURL)
         .setColor("#63798")
@@ -28,17 +11,16 @@ module.exports.run = async (bot, message, args) => {
         let Sembed = new MessageEmbed()
         .setColor("#63798")
         .setAuthor(`Sakura X Help`, message.guild.iconURL)
-        .setThumbnail(bot.user.displayAvatarURL)
+        .setThumbnail(client.user.displayAvatarURL)
         .setTimestamp()
         .setDescription(`These are the avaliable commands for the Sakura X!\nThe bot prefix is: ${prefix}`)
         .addField(`Music:`, "``Play`` ``Search`` ``Queue`` ``Nowplaying`` ``Loop`` ``Lyrics`` ``Skip`` ``Skipto`` ``Pause`` ``Resume`` ``Stop`` ``Shuffle`` ``Volume`` ``Remove``")
         .addField(`Moderation:`, "``Kick`` ``Ban`` ``Lock`` ``prune``")
         .addField("Utility", "``Help`` ``Uptime``")
-        .setFooter("Sakura X || xFallen54x#2399", bot.user.displayAvatarURL)
+        .setFooter("Sakura X || xFallen54x#2399", client.user.displayAvatarURL);
         message.channel.send(embed).then(m => m.delete(10000));
         message.author.send(Sembed)
-    }
-}
+};
 
 module.exports.name = "Help"
 module.exports.aliases = ["h","commands"]
